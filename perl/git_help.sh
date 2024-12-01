43731ae613d27cb24044c76ab9183c988f456d3f
AAHAZRED <451@gmx.eu>
2024-12-01 11:13:21 +0100

    added help script

---
+++ perl/git_help.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+echo "\
+clone.sh - clones a repos and config (arg: github URL).
+connect_to_remote.sh - Connect local git repo to remote (arg: github URL).
+correct_log.sh - Change username and email address in git history (args: see script)
+new_pat.sh - Set new personal access token (PAT) in local git repo (no args).
+new_repo.sh - create new local repos and configs it (arg: user name).
+rel_tag.sh - creates and pushes new (release) tag. (args: tag, message).
+"
+
