9b8df3beaf5abf49dff902701e1e2dbbcf3cea39
AAHAZRED <451@gmx.eu>
2024-12-09 21:01:28 +0100

    completed docu

--- perl/git_help.sh
+++ perl/git_help.sh
@@ -5,7 +5,7 @@ clone.sh             - Clones a github repo and configs it (arg: github URL).
 connect_to_remote.sh - Connect local git repo to remote (arg: github URL).
 correct_log.sh       - Change username and email address in git history (args: see script)
 create_repo.sh       - Create new local repos and configs it (arg: user name).
+new_pat.sh           - Set personal access token (PAT) in local git repo (no args).
 rel_tag.sh           - Creates and pushes new (release) tag. (args: tag, message).
-set_pat.sh           - Set personal access token (PAT) in local git repo (no args).
 "
 
